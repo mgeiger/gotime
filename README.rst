5f522cf3b7eaec6cbe058c152995bcb57cf1cfdb
Matthew J. Geiger <mgeiger@users.noreply.github.com>
2018-10-11 21:57:57 -0400

    Add initial CLI arguments. (#41)

--- README.rst
+++ README.rst
@@ -33,8 +33,37 @@ Features
 * Determine the time it takes to go from one address to a second address
 * Works with a number of services included: Google Maps, Bing Maps, MapQuest
 
+Installation
+------------
+
+Package is hosted via warehouse in the PyPi repository.
+
+You are able to install it via pip::
+
+    pip install gotime
+
+Usage
+-----
+
+Command Line
+~~~~~~~~~~~~
+
+After setting up your virtual environment, you can call by running::
+
+    gotime --start="86 Brattle St., Cambridge, MA 02138" \
+           --end="77 Massachusetts Ave., Cambridge, MA 02139"
+
+
+Import
+~~~~~~
+
+Not properly implemented.
+
 Credits
 -------
+
+A huge shoutout to @mkazin for the project idea and the initial code.
+Though most of the original code has been destroyed, the idea lives on.
 
 This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
 
--- gotime/cli.py
+++ gotime/cli.py
@@ -1,16 +1,43 @@
 # -*- coding: utf-8 -*-
 
 """Console script for gotime."""
+import os
 import sys
+
 import click
+
+from gotime import GoTime, Keys
 
 
 @click.command()
-def main(args=None):
-    """Console script for gotime."""
-    click.echo("Replace this message by putting your code into "
-               "gotime.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('--start', help="The start location")
+@click.option('--end', help="The end location")
+def main(start, end):
+    """
+    Give me a start location and an end location and
+    I will determine how long it will take to get there.
+
+    I will be looking for environmental variables using 
+    the following format:
+
+    `<SERVICE>_API_KEY`
+
+    Where <SERVICE> can be any of the following:
+    
+    * GOOGLE_MAPS\n
+    * BING_MAPS\n
+    * MAPQUEST
+
+    """
+    print(f"Going from {start} to {end}")
+
+    
+    key_list = list()
+    for key in Keys:
+        key_list = (key.name, os.getenv(key.value, None))
+
+    gt = GoTime(key_list)
+
     return 0
 
 
