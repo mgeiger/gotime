2b8bc00fdd5e52dc113238f0bf98e5655a7787cb
Matthew Geiger <matthew.j.geiger@gmail.com>
2022-12-21 14:59:39 -0500

    Update python versions

--- setup.py
+++ setup.py
@@ -26,9 +26,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     description="How long does it take to get from Point A to Point B",
     entry_points={
